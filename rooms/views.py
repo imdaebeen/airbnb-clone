0a76b90fe94a2768a65ed8553e98e445fa6f1bfc
imdaebeen <imdaebeen_09@naver.com>
2021-10-20 00:38:24 +0900

    DetailView Class based add

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from . import models
 
 class HomeView(ListView):
@@ -9,3 +9,17 @@ class HomeView(ListView):
     paginate_by = 10
     paginate_orphans = 5
     ordering = "created"
+    context_object_name = "rooms"
+
+# def room_detail(request, pk):
+#     try:
+#         room = models.Room.objects.get(pk=pk)
+#         return render(request, "rooms/detail.html", {"room": room})
+#     except models.Room.DoesNotExist:
+#         raise Http404()
+
+class RoomDetail(DetailView):
+    
+    """ RoomDetail Definition """
+
+    model = models.Room
